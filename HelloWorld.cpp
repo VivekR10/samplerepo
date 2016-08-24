dfdaae3bcc1f2abb137a8de3ac01410dcf5005bc
Vivek Ramachandran <Vivek.S.Ramachandran@gmail.com>
2016-08-24 15:00:28 -0700

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
 int main() {
-        std::cout << "HelloWorld!" << std::endl;
+        std::cout << "Hello World!" << std::endl;
+        std::cout << "Fight On!" << std::endl;
 }
